b4dbc513d3ecc0d765ac4dbb05599c17c3cf04aa
foxieze <jonah@rowlinson.eu>
2024-06-06 21:27:20 +0100

    attempt at micro

--- mbtilt.py
+++ mbtilt.py
@@ -2,4 +2,4 @@ from microbit import accelerometer
 
 while True:
     x, y, z = accelerometer.get_x(), accelerometer.get_y(), accelerometer.get_z()
-    print(x,y,z)
+    print(str(x) + '_' + str(y) + '_' + str(z))
